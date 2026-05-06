5df697ccf34eeff791cf9019948b4be3b4af4652
Cristiano005 <cristianosilvadesousa05@gmail.com>
2026-05-05 21:47:56 -0300

    feat: header finished

---
+++ src/views/components/Header.jsx
@@ -0,0 +1,30 @@
+export default function Header() {
+
+    return (
+        <header>
+            <nav>
+                <a href="#" className="logo"> CS </a>
+                <div className="links">
+                    <ul>
+                        <li>
+                            <a href="#">About</a>
+                        </li>
+                        <li>
+                            <a href="#">Experience</a>
+                        </li>
+                        <li>
+                            <a href="#">Projects</a>
+                        </li>
+                        <li>
+                            <a href="#">Contact</a>
+                        </li>
+                    </ul>
+                </div>
+                <a href="#" className="contact-me">
+                    Fale comigo
+                </a>
+            </nav>
+        </header>
+    )
+
+}
