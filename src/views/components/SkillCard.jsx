933202d33a3d5e6486a508ab0a78ae5344a1a10e
Cristiano005 <cristianosilvadesousa05@gmail.com>
2026-05-19 22:49:30 -0300

    fix: skill card component improved

--- src/views/components/SkillCard.jsx
+++ src/views/components/SkillCard.jsx
@@ -6,7 +6,7 @@ export default function SkillCard({ skill }) {
             <div className="skill-group-title">
                 {skill.groupSkill}
             </div>
-            {skill.techs.map(tech => <div className="skill-item">
+            {skill.techs.map(tech => <div className="skill-item" key={"skillKey" + tech}>
                 <span className="dot"></span>
                 {tech}
             </div>)}
